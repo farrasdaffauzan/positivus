013401f61bb273a8ad22dbed0662ba6ce359b338
farrasdaffauzan <farrasdaffauzan@gmail.com>
2024-09-17 23:04:09 +0700

    fixed images

--- src/components/Services.js
+++ src/components/Services.js
@@ -22,7 +22,7 @@ function Services() {
               </div>
 
               <div className="absolute top-20 lg:top-10 right-6">
-                <img src="/images/services/illustration.svg" alt=" ilustrasi 1" className="w-[200px] lg:w-[250px]" />
+                <img src="/images/services/Services.svg" alt=" ilustrasi 1" className="w-[200px] lg:w-[250px]" />
               </div>
             </div>
           </div>
@@ -40,7 +40,7 @@ function Services() {
               </div>
 
               <div className="absolute top-24 lg:top-12 right-6">
-                <img src="/images/services/illustration3.svg" alt=" ilustrasi 1" className="w-[200px] lg:w-[250px]" />
+                <img src="/images/services/Services3.svg" alt=" ilustrasi 1" className="w-[200px] lg:w-[250px]" />
               </div>
             </div>
           </div>
@@ -58,7 +58,7 @@ function Services() {
               </div>
 
               <div className="absolute top-10 lg:top-2 right-8">
-                <img src="/images/services/illustration1.svg" alt=" ilustrasi 1" className="w-[200px] lg:w-[230px]" />
+                <img src="/images/services/Services1.svg" alt=" ilustrasi 1" className="w-[200px] lg:w-[230px]" />
               </div>
             </div>
           </div>
@@ -94,7 +94,7 @@ function Services() {
               </div>
 
               <div className="absolute top-16 lg:top-10 right-8 lg:right-12">
-                <img src="/images/services/illustration2.svg" alt=" ilustrasi 1" className="w-[180px] lg:w-[210px]" />
+                <img src="/images/services/Services2.svg" alt=" ilustrasi 1" className="w-[180px] lg:w-[210px]" />
               </div>
             </div>
           </div>
